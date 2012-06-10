8b83af9672fe0bde636c7efec2af99e6782774ad
Jacek <jacek@kuzemczak.co.uk>
2012-06-10 02:58:52 +0100

    Delay loading of locked blocks to give any plugins a chance to create their worlds

---
+++ uk/co/jacekk/bukkit/grouplock/SetBlockMetadataTask.java
@@ -0,0 +1,30 @@
+package uk.co.jacekk.bukkit.grouplock;
+
+import org.bukkit.block.Block;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import uk.co.jacekk.bukkit.baseplugin.BaseTask;
+import uk.co.jacekk.bukkit.grouplock.storage.LockedBlockStorable;
+
+public class SetBlockMetadataTask extends BaseTask<GroupLock> {
+	
+	public SetBlockMetadataTask(GroupLock plugin){
+		super(plugin);
+	}
+	
+	public void run(){
+		for (LockedBlockStorable storedBlock : plugin.lockedBlocks.getAll()){
+			Block block = storedBlock.getBlock();
+			
+			if (!plugin.lockableContainers.contains(block.getType())){
+				plugin.lockedBlocks.remove(block);
+			}else{
+				block.setMetadata("owner", new FixedMetadataValue(plugin, storedBlock.getOwner()));
+				block.setMetadata("allowed", new FixedMetadataValue(plugin, storedBlock.getAllowed()));
+			}
+		}
+		
+		plugin.log.info("Loaded " + plugin.lockedBlocks.size(true) + " locked blocks");
+	}
+	
+}
